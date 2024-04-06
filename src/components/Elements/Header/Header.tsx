fb48002300126e67400caaf8f1880d2b11b1ca4a
vishgits <145033562+vishgits@users.noreply.github.com>
2024-04-06 10:06:24 +0100

    (feat) Header updated

--- src/components/Elements/Header/Header.tsx
+++ src/components/Elements/Header/Header.tsx
@@ -4,8 +4,7 @@ export default function Header() {
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Quinnox</span>
-            <img className="h-8 w-auto" src="https://www.quinnox.com/wp-content/uploads/2022/06/LOGO.svg" alt="" />
+            <span className="sr-only">ImageFinder</span>
           </a>
         </div>
       </nav>
